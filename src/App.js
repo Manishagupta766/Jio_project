6db14a8a5d3e77298e07f770f8040e6860539a66
Anjali834001 <vaidyaanjali023@gmail.com>
2024-08-16 14:25:11 +0530

    commit cgange

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import Canvas from './components/Canvas';
-import WorkflowModal from './components/workflowModal'; // Import your modal if needed
+
 import './App.css';
 import './index.css';
 
